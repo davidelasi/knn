0fc9d3df74c435b337193ab6c07a79ba383dd0f5
davidelasi <davide.lasi@gmail.com>
2018-04-04 10:50:30 -0400

    exercise solution

--- exercise.js
+++ exercise.js
@@ -10,21 +10,41 @@ roomsRange = roomsMax-roomsMin;
 areaRange  = areaMax-areaMin;
 
 exercise.measureDistances = function(rooms,area){
-	// ------------------------
-	// YOUR CODE
-	// ------------------------	
+	data.forEach(function(residence){
+		var deltaRooms = residence.rooms - rooms;
+		deltaRooms = deltaRooms / roomsRange; // normalization
+
+		var deltaArea = residence.area - area;
+		deltaArea = deltaArea / areaRange; // normalization
+	
+		residence.distance = Math.sqrt(deltaRooms * deltaRooms + deltaArea * deltaArea);
+	});
 };
 
 exercise.sortByDistance = function () {
-	// ------------------------
-	// YOUR CODE
-	// ------------------------	
+	data.sort(function(a,b){
+		return a.distance - b.distance;
+
+	});
 };
 
 exercise.guessType = function(k){
-	// ------------------------
-	// YOUR CODE
-	// ------------------------	
+	var types = {apartment:0, flat:0, house:0}; // keep track of these things
+
+	var nn = data.slice(0,k); // k is the number selected to be the nearest neighbors
+
+	nn.forEach(function(item){
+		if(item.type==='apartment') types.apartment +=1;
+		if(item.type==='flat') types.flat +=1;
+		if(item.type==='house') types.house +=1;
+	});
+
+	var type = Object.keys(types).reduce(function(a,b){
+		return types[a] > types[b] ? a : b;
+
+	});
+
+	return{type:type, count:types[type]};
 
 	/*
 		// data format you need to return
